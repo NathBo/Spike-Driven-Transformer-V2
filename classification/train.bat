4bae916d7c6b4be52d8f629d803cc19b67a58d8c
NathBo <nboyer200492@gmail.com>
2026-06-03 16:23:46 +0200

    new lines

--- classification/train.bat
+++ classification/train.bat
@@ -1,1 +1,7 @@
 python main_finetune.py --batch_size 128 --blr 6e-4 --warmup_epochs 10 --epochs 200 --model metaspikformer_8_512 --data_path flowers102_imagenet_like/ --output_dir outputs/55M --log_dir outputs/55M --model_mode ms --dist_eval
+
+export MASTER_ADDR=127.0.0.1
+export MASTER_PORT=29500
+export WORLD_SIZE=1
+export RANK=0
+export LOCAL_RANK=0
