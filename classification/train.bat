86705cd1d5d16c2976679d71ec49b76195d905d7
NathBo <nboyer200492@gmail.com>
2026-06-03 16:51:29 +0200

    new torchrun line

--- classification/train.bat
+++ classification/train.bat
@@ -1,5 +1,8 @@
 python main_finetune.py --batch_size 128 --blr 6e-4 --warmup_epochs 10 --epochs 200 --model metaspikformer_8_512 --data_path flowers102_imagenet_like/ --output_dir outputs/55M --log_dir outputs/55M --model_mode ms --dist_eval
 
+python -m torch.distributed.run --nproc_per_node=8 --master_addr=127.0.0.1 --master_port=29500 main_finetune.py --batch_size 128 --blr 6e-4 --warmup_epochs 10 --epochs 200 --model metaspikformer_8_512 --data_path flowers102_imagenet_like/ --output_dir outputs/55M --log_dir outputs/55M --model_mode ms --dist_eval
+
+
 export MASTER_ADDR=127.0.0.1
 export MASTER_PORT=29500
 export WORLD_SIZE=1
