0201157cc226d16091761cdc840f78d5797dbcd5
NathBo <nboyer200492@gmail.com>
2026-06-15 16:11:40 +0200

    command line imagenet

--- classification/train.bat
+++ classification/train.bat
@@ -2,6 +2,8 @@ srun python main_finetune.py --batch_size 32 --blr 6e-4 --warmup_epochs 10 --epo
 
 srun python -m torch.distributed.run --nproc_per_node=8 --master_addr=127.0.0.1 --master_port=29500 main_finetune.py --batch_size 32 --blr 6e-4 --warmup_epochs 10 --epochs 200 --model metaspikformer_8_512 --data_path flowers102_imagenet_like/ --output_dir outputs/55M --log_dir outputs/55M --model_mode ms --dist_eval --num_workers 8 --nb_classes 102
 
+srun python -m torch.distributed.run --nproc_per_node=1 --master_addr=127.0.0.1 --master_port=29500 main_finetune.py --batch_size 32 --blr 6e-4 --warmup_epochs 0 --epochs 200 --model metaspikformer_8_512 --data_path imagenet_kaggle/ --output_dir outputs/55M --log_dir outputs/55M --model_mode ms --dist_eval --num_workers 8 --nb_classes 1000
+
 # ajuster nproc_per_node en fonction de ce qu'on a de dispo
 
 
